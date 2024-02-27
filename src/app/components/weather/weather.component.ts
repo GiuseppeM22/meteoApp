5bca6ec23ba52a0374aba158678dcca32a87996b
Giuseppe Mongelli <120095798+GiuseppeM22@users.noreply.github.com>
2024-02-27 10:15:31 +0100

    add some control for background image

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -11,11 +11,12 @@ export class WeatherComponent implements OnInit {
   coordinates: { latitude: number, longitude: number } | null = null;
   meteoApi: string = '';
   weatherData: any = null;
+  backgroundImage: string;
+  pageTitle: string = '';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-   
     this.searchCoordinates();
   }
 
@@ -32,23 +33,36 @@ export class WeatherComponent implements OnInit {
           latitude: parseFloat(data[0].lat),
           longitude: parseFloat(data[0].lon),
         };
-
-        this.meteoApi = `https://api.open-meteo.com/v1/forecast?latitude=${this.coordinates.latitude}&longitude=${this.coordinates.longitude}&current=temperature_2m,wind_speed_10m`;
+        this.meteoApi = `https://api.open-meteo.com/v1/forecast?latitude=${this.coordinates.latitude}&longitude=${this.coordinates.longitude}&current=temperature_2m,relative_humidity_2m,precipitation_probability,cloud_cover,wind_speed_10m`;
 
         this.fetchWeatherData();
+        this.pageTitle = `Meteo ${this.cityName}`;
       } else {
         this.coordinates = null;
-        this.weatherData = null; 
+        this.weatherData = null;
+        this.backgroundImage = ''; 
+        this.pageTitle = '';
       }
     });
-
   }
 
   fetchWeatherData(): void {
     if (this.meteoApi) {
       this.http.get<any>(this.meteoApi).subscribe(
         data => {
-          this.weatherData = data.current; 
+          this.weatherData = data.current;
+          // Controllo per impostare la classe per lo sfondo in base alla temperatura
+          if (this.weatherData.temperature_2m <= 0 && this.weatherData.precipitation_probability >= 50) {
+            this.backgroundImage = 'immagineNeve';
+          } else if (this.weatherData.temperature_2m <= 0) {
+            this.backgroundImage = 'immagineFreddo';
+          } else if (this.weatherData.precipitation_probability >= 50) {
+            this.backgroundImage = 'immaginePioggia';
+          } else if (this.weatherData.cloud_cover >= 70) {
+            this.backgroundImage = 'immagineNuvoloso';
+          } else {
+            this.backgroundImage = 'immagineSole';
+          }
         },
         error => {
           console.error('Errore durante la chiamata all\'API del meteo:', error);
